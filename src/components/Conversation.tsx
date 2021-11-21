2ee23d0219e2d2001591098bf40ae4e20b5f2e9f
Alec M <alecmattu1234@gmail.com>
2021-11-21 17:01:14 -0500

    Add chat styling component

--- src/components/Conversation.tsx
+++ src/components/Conversation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CloseButton, InputGroup, FormControl, ButtonGroup, Button, Dropdown, DropdownButton } from 'react-bootstrap';
+import "../chat.css";
 
 export default class Conversation extends React.Component {
   render() {
@@ -10,8 +11,55 @@ export default class Conversation extends React.Component {
             <span className="fs-5 fw-semibold">Ben Mourside</span>
             <CloseButton className="ms-auto" />
           </a>
-          <div className="overflow-auto">
-            <h3>Message Content Goes Here</h3>
+          <div className="overflow-auto chat-history p-3">
+            <ul className="m-b-0">
+              <li className="clearfix">
+                  <div className="message-data text-end">
+                      <span className="message-data-time">10:10 AM, Today</span>
+                      <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar" />
+                  </div>
+                  <div className="message other-message float-end"> Hi Aiden, how are you? How is the project coming along? </div>
+              </li>
+              <li className="clearfix">
+                  <div className="message-data">
+                      <span className="message-data-time">10:12 AM, Today</span>
+                  </div>
+                  <div className="message my-message">Are we meeting today?</div>
+              </li>
+              <li className="clearfix">
+                  <div className="message-data">
+                      <span className="message-data-time">10:15 AM, Today</span>
+                  </div>
+                  <div className="message my-message">Project has been already finished and I have results to show you.</div>
+              </li>
+              <li className="clearfix">
+                  <div className="message-data text-end">
+                      <span className="message-data-time">10:10 AM, Today</span>
+                      <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar" />
+                  </div>
+                  <div className="message other-message float-end"> Hi Aiden, how are you? How is the project coming along? </div>
+              </li>
+              <li className="clearfix">
+                  <div className="message-data">
+                      <span className="message-data-time">10:15 AM, Today</span>
+                  </div>
+                  <div className="message my-message">Project has been already finished and I have results to show you.</div>
+              </li>
+              <li className="clearfix">
+                  <div className="message-data text-end">
+                      <span className="message-data-time">10:10 AM, Today</span>
+                      <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar" />
+                  </div>
+                  <div className="message other-message float-end"> Hi Aiden, how are you? How is the project coming along? </div>
+              </li>
+              <li className="clearfix">
+                  <div className="message-data text-end">
+                      <span className="message-data-time">10:10 AM, Today</span>
+                      <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar" />
+                  </div>
+                  <div className="message other-message float-end"> Hi Aiden, how are you? How is the project coming along? </div>
+              </li>
+            </ul>
           </div>
           <div className="position-absolute bottom-0 start-0 end-0 p-2 bg-white d-flex border-top">
             <InputGroup>
