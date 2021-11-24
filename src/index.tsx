5e92399448ed1c3715faec3998d3b0277793d303
Alec M <alecmattu1234@gmail.com>
2021-11-24 11:17:53 -0500

    Implement basic GroupMe access token storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Dotenv from 'dotenv';
 import App from './App';
+
+// Initialize Config
+Dotenv.config();
+
+/**
+ * URL Query Params
+ *
+ * @type {URLSearchParams}
+ */
+const params : URLSearchParams = new URLSearchParams(window.location.search);
+if (params.get("access_token")) {
+  const date = new Date();
+  date.setTime(date.getTime() + (7*24*60*60*1000));
+  document.cookie = `access_token=${(params.get("access_token") || "")}; expires=${date.toUTCString()}; path=/`;
+}
+
+/**
+ * GroupMe Access Token Cookie
+ *
+ * @type {String?}
+ */
+const accessToken : string = (document.cookie.match(/^(?:.*;)?\s*access_token\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1] || "";
+if (!accessToken) {
+  window.location.href = `https://oauth.groupme.com/oauth/authorize?client_id=${process.env.REACT_APP_GM_CLIENT}`;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App accessToken={accessToken} />
   </React.StrictMode>,
   document.getElementById('root')
 );
