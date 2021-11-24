f56890a82bc30283ef8f17260f960d1c8bcef808
Alec M <alecmattu1234@gmail.com>
2021-11-24 12:07:14 -0500

    Add basic group/chat API implementation

--- src/components/partials/ConversationList.tsx
+++ src/components/partials/ConversationList.tsx
@@ -2,11 +2,41 @@ import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import ConversationListItem from './ConversationListItem';
 
-export default class ConversationList extends React.Component {
+export default class ConversationList extends React.Component<any, any> {
+  constructor(props : object) {
+    super(props);
+    this.state = { data: [] };
+  }
+
+  async componentDidMount() {
+    // Fetch Groups Data
+    const groups = await fetch(`https://api.groupme.com/v3/groups?access_token=${this.props.accessToken}`);
+    const groupsJson = await groups.json();
+
+    // Fetch Chats (DMs) Data
+    const chats = await fetch(`https://api.groupme.com/v3/chats?access_token=${this.props.accessToken}`);
+    const chatsJson = await chats.json();
+
+    // Set Data
+    this.setState({
+      data: {
+        groups: [... groupsJson.response],
+        chats: [... chatsJson.response]
+      }
+    });
+  }
+
   render() {
+    console.log(this.state.data);
+
     return (
       <ListGroup variant="flush" className="border-bottom scrollarea" style={{overflow: "auto"}}>
-        <ConversationListItem />
+        {this.state.data.groups.map((group : object) => (
+          <ConversationListItem isGroup={true} name={"tbd"} date_string={"dt"} author={"aut"} last_message="last mess" />
+        ))}
+        {this.state.data.chats.map((chat : object) => (
+          <ConversationListItem isGroup={false} name={"tbd"} date_string={"dt"} last_message="last mess" />
+        ))}
       </ListGroup>
     );
   }
--- src/components/partials/ConversationListItem.tsx
+++ src/components/partials/ConversationListItem.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 
-export default class ConversationListItem extends React.Component {
+export default class ConversationListItem extends React.Component<any, any> {
+  constructor(props : object) {
+    super(props);
+  }
+
   render() {
     return (
       <ListGroup.Item action className="py-3 lh-tight">
         <div className="d-flex w-100 align-items-center justify-content-between">
-          <strong className="mb-1">Ben Mourside</strong>
-          <small>Today</small>
+          <strong className="mb-1">{this.props.name}</strong>
+          <small>{this.props.date_string}</small>
         </div>
-        <div className="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+        <div className="col-10 mb-1 small">
+          <b>{this.props.isGroup ? this.props.author : ""}</b>:
+          {this.props.last_message}
+        </div>
       </ListGroup.Item>
     );
   }
