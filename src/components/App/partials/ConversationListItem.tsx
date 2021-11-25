26de3d8f89331cc814d6a7255860b3068f0c48b4
Alec M <alecmattu1234@gmail.com>
2021-11-25 15:38:49 -0500

    Use YYYY-MM-DD date formatting

--- src/components/App/partials/ConversationListItem.tsx
+++ src/components/App/partials/ConversationListItem.tsx
@@ -16,7 +16,7 @@ const ConversationListItem = (props: any): JSX.Element => {
     <ListGroup.Item action active={active} className="py-3 lh-tight" onClick={handleClick}>
       <div className="d-flex w-100 align-items-center justify-content-between">
         <strong className="mb-1">{name}</strong>
-        <small>{message_date.getDay()}</small>
+        <small>{message_date.toISOString().split('T')[0]}</small>
       </div>
       <div className="col-10 mb-1 small">
         <b>{isGroup ? `${author}: ` : ""}</b>
