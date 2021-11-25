142ef2b4c5b6bdd3f6f4c211b5cc147a78f9c412
Alec M <alecmattu1234@gmail.com>
2021-11-25 14:29:19 -0500

    Simplify App component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -9,21 +9,6 @@ import Conversation from './Conversation';
 import ConversationFiller from './ConversationFiller';
 
 /**
-* Fetch conversation
-*
-* @author Alec M. <https://amattu.com>
-* @date 2021-11-24T15:43:41-050
-*/
-const changeConversation = async (accessToken : string, id : string, isGroup : boolean) : Promise<any> => {
- // Variables
- const url = isGroup ? `https://api.groupme.com/v3/groups/${id}/messages?access_token=${accessToken}` :
-   `https://api.groupme.com/v3/direct_messages?other_user_id=${id}&access_token=${accessToken}`
- const data = await fetch(url);
- const dataJson = await data.json();
- console.log(dataJson);
-}
-
-/**
  * Main React App
  */
 const App = (props: any) : JSX.Element => {
@@ -32,11 +17,10 @@ const App = (props: any) : JSX.Element => {
       <AuthProvider>
         <div className="d-flex h-100">
           <Sidebar />
-            <Routes>
-              <Route path='/' element={<ConversationFiller />} />
-              <Route path='/conversation/:id' element={<Conversation />} />
-              <Route path='/group/:id' element={<Conversation />} />
-            </Routes>
+          <Routes>
+            <Route path='/' element={<ConversationFiller />} />
+            <Route path='/:type/:id' element={<Conversation />} />
+          </Routes>
         </div>
       </AuthProvider>
     </HashRouter>
