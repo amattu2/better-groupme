54ab0afde9417f9d8d3e44d452dd19d4b6b48bf3
Alec M <alecmattu1234@gmail.com>
2021-11-26 08:06:37 -0500

    Remove ConversationListHeader component

--- src/components/App/partials/SideConversations.tsx
+++ src/components/App/partials/SideConversations.tsx
@@ -1,6 +1,5 @@
 // Imports
 import React from 'react';
-import ConversationListHeader from './ConversationListHeader';
 import FilterTabs from './FilterTabs';
 
 /**
@@ -9,7 +8,9 @@ import FilterTabs from './FilterTabs';
 const SideConversations = (props: any): JSX.Element => {
   return (
     <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white border-end" style={{width: "350px", zIndex: 998}}>
-      <ConversationListHeader />
+      <div className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+        <span className="fs-5 fw-semibold">Conversations</span>
+      </div>
       <FilterTabs />
     </div>
   );
