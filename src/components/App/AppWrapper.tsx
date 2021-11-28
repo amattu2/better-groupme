e8f07b6e82c2bd9a11482a07282f7d2b95f9e4de
Alec M <alecmattu1234@gmail.com>
2021-11-28 10:58:42 -0500

    Move BotListProvider to higher element

--- src/components/App/AppWrapper.tsx
+++ src/components/App/AppWrapper.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import { useAuth } from '../AuthProvider';
 import { UserProfileProvider } from '../DataProviders/UserProfile';
 import { ConversationListProvider } from '../DataProviders/ConversationList';
+import { BotListProvider } from '../DataProviders/BotList';
 import ConversationWrapper from './ConversationWrapper';
 import ConversationFiller from './ConversationFiller';
 import SideNavigation from './partials/SideNavigation';
@@ -20,32 +21,37 @@ const AppWrapper = (props: any): JSX.Element => {
   return (
     <UserProfileProvider token={accessToken}>
       <ConversationListProvider token={accessToken}>
-        {/* Main Container */}
-        <div className="d-flex h-100">
-
-          {/* Navigation */}
+        <BotListProvider token={accessToken}>
+          {/* Main Container */}
           <div className="d-flex h-100">
-            <SideNavigation />
+
+            {/* Navigation */}
+            <div className="d-flex h-100">
+              <SideNavigation />
+              <Routes>
+                <Route path='/' element={<SideConversations />} />
+                <Route path='/group/:id' element={<SideConversations />} />
+                <Route path='/conversation/:id' element={<SideConversations />} />
+                <Route path='/contacts' element={<SidebarFiller />} />
+                <Route path='/archive' element={<SidebarFiller />} />
+                <Route path='/bots' element={<SideBots />} />
+                <Route path='/bots/:id' element={<SideBots />} />
+              </Routes>
+            </div>
+
+            {/* Container Element */}
             <Routes>
-              <Route path='/' element={<SideConversations />} />
-              <Route path='/group/:id' element={<SideConversations />} />
-              <Route path='/conversation/:id' element={<SideConversations />} />
-              <Route path='/contacts' element={<SidebarFiller />} />
-              <Route path='/archive' element={<SidebarFiller />} />
-              <Route path='/bots' element={<SideBots />} />
+              <Route path='/' element={<ConversationFiller />} />
+              <Route path='/group/:id' element={<ConversationWrapper />} />
+              <Route path='/conversation/:id' element={<ConversationWrapper />} />
+              <Route path='/bots' element={<h3>Bots</h3>} />
+              <Route path='/bots/:id' element={<h3>Bot id</h3>} />
+              <Route path='/contacts' element={<h3>contacts</h3>} />
+              <Route path='/archive/' element={<h3>archive</h3>} />
+              <Route path='/settings/' element={<h3>settings</h3>} />
             </Routes>
           </div>
-
-          {/* Container Element */}
-          <Routes>
-            <Route path='/' element={<ConversationFiller />} />
-            <Route path='/:type/:id' element={<ConversationWrapper />} />
-            <Route path='/bots' element={<h3>Bots</h3>} />
-            <Route path='/contacts' element={<h3>contacts</h3>} />
-            <Route path='/archive/' element={<h3>archive</h3>} />
-            <Route path='/settings/' element={<h3>settings</h3>} />
-          </Routes>
-        </div>
+        </BotListProvider>
       </ConversationListProvider>
     </UserProfileProvider>
   );
--- src/components/App/partials/SideBots.tsx
+++ src/components/App/partials/SideBots.tsx
@@ -2,8 +2,6 @@
 import React from 'react';
 import { ListGroup, Button } from 'react-bootstrap';
 import { BsPatchPlus } from 'react-icons/bs';
-import { BotListProvider } from '../../DataProviders/BotList';
-import { useAuth } from '../../AuthProvider';
 import BotList from './BotList';
 
 /**
@@ -38,22 +36,18 @@ class ErrorBoundary extends React.Component<any, any> {
  * Conversation list filter pills
  */
 const SideBots = (props: any): JSX.Element => {
-  const { accessToken } : any = useAuth();
-
   return (
-    <BotListProvider token={accessToken}>
-      <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white border-end" style={{width: "350px", zIndex: 998}}>
-        <div className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
-          <span className="fs-5 fw-semibold">My Bots</span>
-          <Button variant="outline-primary" className="ms-auto">
-            <BsPatchPlus />
-          </Button>
-        </div>
-        <ErrorBoundary>
-          <BotList />
-        </ErrorBoundary>
+    <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white border-end" style={{width: "350px", zIndex: 998}}>
+      <div className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+        <span className="fs-5 fw-semibold">My Bots</span>
+        <Button variant="outline-primary" className="ms-auto">
+          <BsPatchPlus />
+        </Button>
       </div>
-    </BotListProvider>
+      <ErrorBoundary>
+        <BotList />
+      </ErrorBoundary>
+    </div>
   );
 };
 
